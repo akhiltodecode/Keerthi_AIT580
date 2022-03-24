c87c8d461a943fdaef185cce27176b3847bc5584
akhiltodecode <44576706+akhiltodecode@users.noreply.github.com>
2022-03-24 11:40:36 -0400

    Update Keerthi_G01353729.R
    updated file

--- Keerthi_G01353729.R
+++ Keerthi_G01353729.R
@@ -1,37 +1,5 @@
 <<<<<<< HEAD
-=======
-<<<<<<<< HEAD:Keerthi_G01353729.R
->>>>>>> aa416a858c0a13988c2b58a1f14c0482f3cd9f27
-###--------------------------------------
-#Student Name: Akhilesh Keerthi
-#GNumber: G01353729
-###--------------------------------------
 
-library(dplyr)
-library(tidyverse)
-rm(list=ls())
-
-data <- read.csv("C:\\Users\\akhilesh\\Desktop\\GitWork\\AIT580\\data\\EmployeeAttrition.csv")
-# this is just for testing to use print statement
-print(data[1,])
-
-#a. Find the number of rows and columns in the dataset (5 points)
-print("Number of rows are:")
-print(nrow(data))
-print("Number of columns are: ")
-print(ncol(data))
-
-# b. Find the maximum Age in the dataset (5 points)
-max(data$Age)
-print(paste0("The Maximum age in dataset: ", max(data$Age)))
-
-# c. Find the minimum DailyRate in the dataset (5 points)
-min(data$DailyRate)
-print(paste0("The Minimum DailyRate in dataset: ", min(data$DailyRate)))
-
-# d. Find the average/mean MontlyIncome in the dataset (5 points)
-mean(data$MonthlyIncome)
-print(paste0("The Average of MonthlyIncome in dataset: ", mean(data$MonthlyIncome)))
 
 # e. How many employees rated WorkLifeBalance as 1 (5 points)
 nrow(data[data$WorkLifeBalance==1,])
@@ -151,45 +119,5 @@ print("Answer: The correlation coefficient of males and females is between 0.5 a
 
 
 
-<<<<<<< HEAD
-=======
-========
-###--------------------------------------
-#Student Name:
-#GNumber:
-###--------------------------------------
-
-rm(list=ls())
-
-data <- read.csv('https://github.com//akhiltodecode//Keerthi_AIT580//tree//main//Assignment-3_Keerthi_Akhilesh//EmployeeAttrition.csv')
-
-# this is just for testing to use "print" statement.
-print(data[1,])
 
 
-
-# a. Find the number of rows and columns in the dataset (5 points)
-
-
-# b. Find the maximum Age in the dataset (5 points)
-
-
-# c. Find the minimum DailyRate in the dataset (5 points)
-
-
-# d. Find the average/mean MontlyIncome in the dataset (5 points)
-
-
-# e. How many employees rated WorkLifeBalance as 1 (5 points)
-
-
-# f. What percent of total employees have TotalWorkingYears less than equal to 5? Also calculate the percentage for TotalWorkingYears greater than 5 (5 points)
-
-
-# g. Print EmployeeNumber, Department and MaritalStatus for those employees whose Attrition is Yes and RelationshipSatisfaction is 1 and YearsSinceLastPromotion is greater than 3 (10 points)
-
-
-# h. Find the mean, median, mode, standard deviation and frequency distribution of EnvironmentSatisfaction for males and females separately. (Hint: For frequency distribution use table() function (10 points)
- 
->>>>>>>> aa416a858c0a13988c2b58a1f14c0482f3cd9f27:Assignment-3_Keerthi_Akhilesh/Keerthi_G01353729.R
->>>>>>> aa416a858c0a13988c2b58a1f14c0482f3cd9f27
